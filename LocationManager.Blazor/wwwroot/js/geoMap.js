46ee9ac055d74b3c05855ecadf5d60b55530c1ed
ChessWizard <chesswizard2002@gmail.com>
2023-08-08 02:18:12 +0300

    feat: GeoMap Component Added

---
+++ LocationManager.Blazor/wwwroot/js/geoMap.js
@@ -0,0 +1,11 @@
+﻿
+function createLocationMap(latitude,longitude) {
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map);// mevcut konuma marker(işaretçi) ekler
+}
